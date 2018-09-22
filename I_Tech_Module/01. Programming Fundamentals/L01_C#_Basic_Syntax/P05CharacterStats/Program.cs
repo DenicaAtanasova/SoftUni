2d67053024e984809fdad7bf77e30b235f11685d
Denica Dimitrova Atanasova <32368413+DenicaAtanasova@users.noreply.github.com>
2018-09-22 18:05:45 +0300

    Tech Module - September 2017

---
+++ I_Tech_Module/01. Programming Fundamentals/L01_C#_Basic_Syntax/P05CharacterStats/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task5CharacterStats
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maximumHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maximumEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+
+            Console.Write("Health: ");
+            Console.Write(new string('|', currentHealth + 1));
+            Console.Write(new string('.', maximumHealth - currentHealth));
+            Console.WriteLine('|');
+
+            Console.Write("Energy: ");
+            Console.Write(new string('|', currentEnergy + 1));
+            Console.Write(new string('.', maximumEnergy - currentEnergy));
+            Console.WriteLine('|');
+        }
+    }
+}
