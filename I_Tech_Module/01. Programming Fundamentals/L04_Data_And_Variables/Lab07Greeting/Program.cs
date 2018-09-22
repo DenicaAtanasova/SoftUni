2593834f5d40680a203ca44bac5adc88de6eb029
Denica Dimitrova Atanasova <32368413+DenicaAtanasova@users.noreply.github.com>
2018-09-22 18:59:48 +0300

    Tech Module - September 2017

---
+++ I_Tech_Module/01. Programming Fundamentals/L04_Data_And_Variables/Lab07Greeting/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab7Greeting
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Hello, {firstName} {lastName}. \r\n You are {age} years old.");
+        }
+    }
+}
---
+++ I_Tech_Module/01. Programming Fundamentals/L04_Data_And_Variables/Lab09RefactorSpecialNums/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lab9RefactorSpecialNums
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int num = 1; num <= n; num++)
+            {
+                int digit = num;
+                int sum = 0;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num = num / 10;
+                }
+                bool specialNum = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{digit} -> {specialNum}");
+                num = digit;
+            }
+        }
+    }
+}
