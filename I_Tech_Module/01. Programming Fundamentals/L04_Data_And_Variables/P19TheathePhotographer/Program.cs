c0e185c3f280249179d282c48036604bfbff5734
Denica Dimitrova Atanasova <32368413+DenicaAtanasova@users.noreply.github.com>
2018-09-22 19:08:09 +0300

    Tech Module - September 2017

---
+++ I_Tech_Module/01. Programming Fundamentals/L04_Data_And_Variables/P19TheathePhotographer/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace P19TheathePhotographer
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            long picsCount = long.Parse(Console.ReadLine());
+            long filterTime = long.Parse(Console.ReadLine());
+            long filterFactor = long.Parse(Console.ReadLine());
+            long uploadTime = long.Parse(Console.ReadLine());
+
+            long filteredPics = (long) Math.Ceiling(picsCount * filterFactor / 100.0);
+            long allPicsFilterTime = filterTime * picsCount;
+            long allPicsUploadTime = uploadTime * filteredPics;
+
+            long totalTIme = allPicsFilterTime + allPicsUploadTime;
+
+            TimeSpan result = TimeSpan.FromSeconds(totalTIme);
+
+            string format = result.ToString(@"d\:hh\:mm\:ss");
+            Console.WriteLine(format);
+        }
+    }
+}
