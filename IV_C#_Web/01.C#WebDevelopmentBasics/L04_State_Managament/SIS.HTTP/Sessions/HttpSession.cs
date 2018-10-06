2c2a8b69fddf46b86a5fcd76323cb82a50417071
Denica Atanasova <d.d.atanasova@gmail.com>
2018-10-06 17:25:21 +0300

    new Server with cookies and session

---
+++ IV_C#_Web/01.C#WebDevelopmentBasics/L04_State_Managament/SIS.HTTP/Sessions/HttpSession.cs
@@ -0,0 +1,49 @@
+﻿
+namespace SIS.HTTP.Sessions
+{
+    using SIS.HTTP.Sessions.Contracts;
+    using System.Collections.Generic;
+
+    public class HttpSession : IHttpSession
+    {
+        private readonly Dictionary<string, object> parameters;
+
+        public HttpSession(string id)
+        {
+            this.Id = id;
+            this.parameters = new Dictionary<string, object>();
+        }
+
+        public string Id { get; }
+
+        public void AddParameter(string name, object parameter)
+        {
+            if (!this.ContainsParameter(name))
+            {
+                this.parameters[name] = null;
+            }
+
+            this.parameters[name] = parameter;
+        }
+
+        public void ClearParameters()
+        {
+            this.parameters.Clear();
+        }
+
+        public bool ContainsParameter(string name)
+        {
+            return this.parameters.ContainsKey(name);
+        }
+
+        public object GetParameter(string name)
+        {
+            if (!this.ContainsParameter(name))
+            {
+                return null;
+            }
+
+            return this.parameters[name];
+        }
+    }
+}
