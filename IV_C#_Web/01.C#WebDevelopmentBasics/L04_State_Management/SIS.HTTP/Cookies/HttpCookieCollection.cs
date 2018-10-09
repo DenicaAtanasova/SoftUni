8780d1b78298dfc7912e9557bd920dc27e75b537
Denica Atanasova <d.d.atanasova@gmail.com>
2018-10-09 08:52:42 +0300

    Simple spelling mistake fixed

---
+++ IV_C#_Web/01.C#WebDevelopmentBasics/L04_State_Management/SIS.HTTP/Cookies/HttpCookieCollection.cs
@@ -0,0 +1,52 @@
+﻿
+namespace SIS.HTTP.Cookies
+{
+    using SIS.HTTP.Cookies.Contracts;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class HttpCookieCollection : IHttpCookieCollection
+    {
+        private readonly Dictionary<string, HttpCookie> cookies;
+
+        public HttpCookieCollection()
+        {
+            this.cookies = new Dictionary<string, HttpCookie>();
+        }
+
+        public void Add(HttpCookie cookie)
+        {
+            if (!this.ContainsCookie(cookie.Key))
+            {
+                this.cookies[cookie.Key] = null;
+            }
+
+            this.cookies[cookie.Key] = cookie;
+        }
+
+        public bool ContainsCookie(string key)
+        {
+            return this.cookies.ContainsKey(key);
+        }
+
+        public HttpCookie GetCookie(string key)
+        {
+            if (!this.ContainsCookie(key))
+            {
+                return null;
+            }
+
+            return this.cookies[key];
+        }
+
+        public bool HasCookies()
+        {
+            return cookies.Any();
+        }
+
+        public override string ToString()
+        {
+            return string.Join("; ", this.cookies.Values);
+        }
+    }
+}
