eed68bd091c19827403b28c7f4271c34115313a9
Denica Atanasova <d.d.atanasova@gmail.com>
2018-12-28 12:14:44 +0200

    Finished exercises 1 - 8

---
+++ II_C#_Fundamentals/01_C#_Advanced/L02_Multidimensional_Arrays/P03_Squares_In_Matrix/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_Squares_In_Matrix
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var size = Console.ReadLine()
+                .Split()
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            var matrix = new string[size[0], size[1]];
+
+            var squaresFound = 0;
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                var elements = Console.ReadLine()
+                    .Split();
+
+                matrix[rows, 0] = elements[0];
+
+                for (int cols = 1; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] = elements[cols];
+
+                    if (rows != 0)
+                    {
+                        if (matrix[rows, cols] == matrix[rows - 1, cols] &&
+                            matrix[rows, cols] == matrix[rows, cols - 1] &&
+                            matrix[rows, cols] == matrix[rows - 1, cols - 1])
+                        {
+                            squaresFound++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(squaresFound);
+        }
+    }
+}
---
+++ II_C#_Fundamentals/01_C#_Advanced/L02_Multidimensional_Arrays/P07_Lego_Blocks/StartUp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+namespace P07_Lego_Blocks
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var cellsCounter = new int[n];
+
+            var firstBlock = ArrangeBlock(1, n);
+            var secondBlock = ArrangeBlock(2, n);
+
+            FitBlocks();
+
+            string[][] ArrangeBlock(int arrayNumber, int rows)
+            {
+                var block = new string[rows][];
+
+                for (int row = 0; row < rows; row++)
+                {
+                    block[row] = GetInput(arrayNumber);
+
+                    cellsCounter[row] += block[row].Length;
+                }
+
+                return block;
+            }
+
+            void FitBlocks()
+            {
+                var doBlocksFit = true;
+
+                for (int i = 1; i < n; i++)
+                {
+                    if (cellsCounter[i] != cellsCounter[i - 1])
+                    {
+                        doBlocksFit = false;
+                    }
+
+                }
+
+                if (doBlocksFit)
+                {
+                    for (int row = 0; row < n; row++)
+                    {
+                        Console.WriteLine("[{0}]", string.Join(", ",
+                                                    string.Join(", ", firstBlock[row]),
+                                                    string.Join(", ", secondBlock[row])));
+                    }
+                }
+                else
+                {
+                    var cellsCount = cellsCounter.Sum();
+                    Console.WriteLine($"The total number of cells is: {cellsCount}");
+                }
+            }
+        }
+
+        private static string[] GetInput(int arrayNumber)
+        {
+            var input = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            if (arrayNumber == 2)
+            {
+                Array.Reverse(input);
+
+                return input;
+            }
+
+            return input;
+        }
+    }
+}
