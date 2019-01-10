a9aa7a24be2de700c12ee0da8dee2f52bfd78e8c
Denica Atanasova <d.d.atanasova@gmail.com>
2019-01-10 11:06:35 +0200

    Finished exercises from 1 to 8. Problem 9 - HTTP Server left unsolved.

---
+++ II_C#_Fundamentals/01_C#_Advanced/L04_Streams/P01_Odd_Lines/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System;
+
+namespace P01_Odd_Lines
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lineNummber = 1;
+
+            var file = "../../../../Resources/text.txt";
+            using (var reader = new StreamReader(file))
+            {
+                string line = string.Empty;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (lineNummber % 2 == 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    lineNummber++;
+                }
+            }
+        }
+    }
+}
---
+++ II_C#_Fundamentals/01_C#_Advanced/L04_Streams/P07_Directory_Traversal/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P07_Directory_Traversal
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var directoryInfo = new Dictionary<string, Dictionary<string, double>>();
+            var files = Directory.GetFiles(".", "*.*", SearchOption.TopDirectoryOnly);
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+
+                var name = fileInfo.Name;
+                var extension = fileInfo.Extension;
+                var size = fileInfo.Length * 0.001;
+
+                if (!directoryInfo.ContainsKey(extension))
+                {
+                    directoryInfo.Add(extension, new Dictionary<string, double>());
+                }
+                
+                directoryInfo[extension].Add(name, size);               
+            }
+
+            var resultPath = "../../../../Results/report.txt";
+            //"C:/Users/Public/Desktop/report.txt";
+            using (var writer = new StreamWriter(resultPath))
+            {
+                foreach (var ext in directoryInfo.OrderByDescending(x => x.Value.Keys.Count).ThenBy(x => x.Key))
+                {
+                    writer.WriteLine($"{ext.Key}");
+
+                    foreach (var file in ext.Value.OrderBy(x => x.Value))
+                    {
+                        writer.WriteLine($"--{file.Key} - {file.Value:F3}kb");
+                    }
+                }
+            }
+        }
+    }
+}
