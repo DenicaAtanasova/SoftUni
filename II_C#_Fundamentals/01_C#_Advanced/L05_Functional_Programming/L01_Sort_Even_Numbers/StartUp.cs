bae60ffbb27a062428fda59bc51a7303b8985d44
Denica Atanasova <d.d.atanasova@gmail.com>
2019-01-12 08:28:10 +0200

    Functional programming labs finished

---
+++ II_C#_Fundamentals/01_C#_Advanced/L05_Functional_Programming/L01_Sort_Even_Numbers/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace L01_Sort_Even_Numbers
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(x=>x % 2 == 0)
+                .OrderBy(x => x)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
---
+++ II_C#_Fundamentals/01_C#_Advanced/L05_Functional_Programming/L03_Count_Uppercase_Words/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace L03_Count_Uppercase_Words
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> isUpper = s => char.IsUpper(s[0]);
+
+            var words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(isUpper);
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
