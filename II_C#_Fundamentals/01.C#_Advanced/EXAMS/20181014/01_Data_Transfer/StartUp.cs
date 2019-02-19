51236ecc4592e8d58986f476e87e01c908537e53
Denica Atanasova <d.d.atanasova@gmail.com>
2019-02-19 10:59:10 +0200

    Data transfer problem finished at 100/100

--- II_C#_Fundamentals/01.C#_Advanced/EXAMS/20181014/01_Data_Transfer/StartUp.cs
+++ II_C#_Fundamentals/01.C#_Advanced/EXAMS/20181014/01_Data_Transfer/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _01_Data_Transfer
 {
@@ -6,7 +8,45 @@ namespace _01_Data_Transfer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var n = int.Parse(Console.ReadLine());
+            var dataTransferSize = 0;
+
+            while (n-- >0)
+            {
+                var input = Console.ReadLine().Split(';',  StringSplitOptions.RemoveEmptyEntries);
+
+                if(input.Length < 3)
+                {
+                    continue;
+                }
+
+                var message = string.Empty;
+                var messsagePattern = "^m--(\"[A-Za-z\\s]+\")$";
+                if (Regex.IsMatch(input[2], messsagePattern))
+                {
+                    message = input[2].TrimStart('m', '-');
+
+                    var namePattern = @"[A-Za-z\s]+";
+                    if(input[0].Substring(0,2) != "s:" || input[1].Substring(0, 2) != "r:")
+                    {
+                        continue;
+                    }
+
+                    var sender = string.Join("", Regex.Matches(input[0].TrimStart('s', ':'), namePattern));
+                    var receiver = string.Join("", Regex.Matches(input[1].TrimStart('r', ':'), namePattern));
+
+                    Console.WriteLine($"{sender} says {message} to {receiver}");
+                    dataTransferSize += Regex.Matches(input[0], "\\d")
+                            .Cast<Match>()
+                            .Select(m => int.Parse(m.Value)).Sum();
+
+                    dataTransferSize += Regex.Matches(input[1], "\\d")
+                            .Cast<Match>()
+                            .Select(m => int.Parse(m.Value)).Sum();
+                }
+            }
+
+            Console.WriteLine($"Total data transferred: {dataTransferSize}MB");
         }
     }
 }
