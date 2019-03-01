80d208b717683f1510dd55239e2b85c8bfe59cb5
Denica Atanasova <d.d.atanasova@gmail.com>
2019-03-01 06:38:14 +0200

    Added warehouse solved at 81/100

--- V_JS_Core/02.JS_Advanced/L03_Classes/P01_Data_Class.js
+++ V_JS_Core/02.JS_Advanced/L03_Classes/P01_Data_Class.js
@@ -1,22 +1,11 @@
-// class Request {
-//     constructor(method, uri, version, message){
-//         this.method = method;
-//         this.uri = uri;
-//         this.version = version;
-//         this.message = message;
-//         this.response = undefined;
-//         this.fulfilled = false;
-//     }
-// }
-
-function some(){
-    console.log(this.name);
+class Request {
+    constructor(method, uri, version, message){
+        this.method = method;
+        this.uri = uri;
+        this.version = version;
+        this.message = message;
+        this.response = undefined;
+        this.fulfilled = false;
+    }
 }
 
-let a = (function(){
-    return console.log(123456);
-})();
-
-let ba = a;
-ba()
-a
